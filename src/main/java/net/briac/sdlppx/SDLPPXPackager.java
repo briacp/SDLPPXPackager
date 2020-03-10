d93da9d0bede8f16f37aeb3b1f19784288b18b15
Briac Pilpré <briacp@gmail.com>
2020-03-10 20:10:39 +0100

    CLI, SDLTM, SDLTB

--- src/main/java/net/briac/sdlppx/SDLPPXPackager.java
+++ src/main/java/net/briac/sdlppx/SDLPPXPackager.java
@@ -1,5 +1,5 @@
 /**************************************************************************
- Copyright (C) 2019 Briac Pilpré
+ Copyright (C) 2020 Briac Pilpré
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -18,6 +18,7 @@ package net.briac.sdlppx;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -32,9 +33,11 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -48,6 +51,11 @@ import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -55,55 +63,132 @@ import org.xml.sax.SAXException;
 
 /**
  * Convert a Trados SDLPPX Project Package to SDLRPX Return Package.
- * 
+ *
  * Original idea from Fi2Pro video : https://www.youtube.com/watch?v=a4ZGeAjTl2M
- * 
+ *
  * @author briac
  *
  */
 public class SDLPPXPackager {
+
     private static final String TARGET_DIR = "target";
     private static final String SOURCE_DIR = "source";
+    private static final String TM_DIR = "tm";
+    private static final String GLOSSARY_DIR = "glossary";
+    private static final String HELP_LINE = "SDLPPXPackager [options] --project-dir project_dir sdlppx";
 
-    private static final Logger LOGGER = Logger.getLogger(SDLPPXPackager.class.getName());
+    private final Path sdlPpx;
+    private String targetLanguage;
+    private Document sdlProjDoc;
+    private boolean noGlossary = false;
+    private boolean noTMX = false;
+    private boolean noSource = false;
 
     private static final String ATTRIBUTE_PACKAGE_TYPE = "PackageType";
+    private static final int MAX_DEPTH = 10;
+
+    private final DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+
+    static {
+        System.setProperty("java.util.logging.SimpleFormatter.format",
+                "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$-6s %2$s %5$s%6$s%n");
+    }
+
+    private static final Logger LOGGER = Logger.getLogger(SDLPPXPackager.class.getName());
 
     public static enum PackageTypes {
         ProjectPackage, ReturnPackage
     }
 
-    private final Path sdlPpx;
-    private final Path projectDir;
-    private String targetLanguage;
-
-    public SDLPPXPackager(String filename, String projectDir) throws FileNotFoundException {
-        this.sdlPpx = Paths.get(filename);
-        this.projectDir = Paths.get(projectDir);
-        if (!sdlPpx.toFile().exists()) {
+    public SDLPPXPackager(String sdlPpx) throws FileNotFoundException {
+        this.sdlPpx = Paths.get(sdlPpx);
+        if (!this.sdlPpx.toFile().exists()) {
+            LOGGER.log(Level.SEVERE, "SDLPPX file {0} does not exist.", new Object[] {sdlPpx});
             throw new FileNotFoundException("SDLPPX file does not exist.");
         }
-        if (!this.projectDir.toFile().exists()) {
-            throw new FileNotFoundException("Project directory does not exist.");
-        }
-
     }
 
     public static void main(String[] args) throws Throwable {
-        if (args.length < 2) {
+        // CLI mode
+        Options options = new Options();
+        options.addOption("h", "help", false, "print this message");
+        options.addOption("G", "gui", false, "force the GUI mode");
+        options.addOption("p", "project-dir", true, "project directory");
+        options.addOption("e", "extract", false, "extract source files from the SDLPPX");
+        options.addOption("r", "return", false, "create the return SDLPRX package (default)");
+        options.addOption("ng", "no-glossary", false, "skip the SDLTB glossary extraction");
+        options.addOption("nt", "no-tm", false, "skip the SDLTM memory extraction");
+        options.addOption("ns", "no-source", false, "skip the SDLXLIFF sources extraction");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+        
+        if (args.length == 0 || cmd.hasOption("G")) {
             SDLPPXPackagerWindow win = new SDLPPXPackagerWindow();
+            
+            if (cmd.hasOption("p")) {
+                win.targetDir.setText(cmd.getOptionValue("p"));
+            }
+            if (! cmd.getArgList().isEmpty()) {
+                win.sdlppxFile.setText(cmd.getArgList().get(0));
+            }
+            if (cmd.hasOption("ng")) {
+                win.cbSkipGlossary.setSelected(true);
+            }
+            if (cmd.hasOption("nt")) {
+                win.cbSkipTM.setSelected(true);
+            }
+            if (cmd.hasOption("ns")) {
+                win.cbSkipSources.setSelected(true);
+            }
+                
             win.setVisible(true);
-        } else {
-            new SDLPPXPackager(args[0], args[1]).updateSdlppx();
+            return;
         }
+
+        HelpFormatter formatter = new HelpFormatter();
+        if (cmd.hasOption("h")) {
+            formatter.printHelp(HELP_LINE, options);
+            System.exit(2);
+        }
+
+        if (cmd.getArgList().isEmpty()) {
+            System.err.println("Missing required 'sdlppx' file parameter");
+            formatter.printHelp(HELP_LINE, options);
+            System.exit(3);
+        }
+        if (! cmd.hasOption("p")) {
+            System.err.println("Missing required 'project-dir' parameter.");
+            formatter.printHelp(HELP_LINE, options);
+            System.exit(4);
+        }
+
+        File f = new File(cmd.getArgList().get(0));
+        SDLPPXPackager sdl = new SDLPPXPackager(f.getAbsolutePath());
+        if (cmd.hasOption("ng")) {
+            sdl.noGlossary = true;
+        }
+        if (cmd.hasOption("nt")) {
+            sdl.noTMX = true;
+        }
+        if (cmd.hasOption("ns")) {
+            sdl.noSource = true;
+        }
+
+        if (cmd.hasOption("extract")) {
+            sdl.extractSource(cmd.getOptionValue("p"));
+        } else {
+            sdl.updateSdlppx(cmd.getOptionValue("p"));
+        }
+
     }
 
-    public boolean extractSource() throws Exception {
+    public boolean extractSource(String projectDir) throws Exception {
 
         PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:*.sdlproj");
 
         try (FileSystem zipfs = FileSystems.newFileSystem(sdlPpx, null)) {
-            Stream<Path> sdlProjStream = Files.find(zipfs.getRootDirectories().iterator().next(), 1,
+            Stream<Path> sdlProjStream = Files.find(zipfs.getRootDirectories().iterator().next(), MAX_DEPTH,
                     (path, basicFileAttributes) -> matcher.matches(path));
             Path sdlProj = sdlProjStream.findFirst().get();
             sdlProjStream.close();
@@ -111,15 +196,35 @@ public class SDLPPXPackager {
             LOGGER.log(Level.INFO, "SDLProj file: {0}", sdlProj);
             parseSDLProj(sdlProj, false);
 
-            File sourceDir = new File(projectDir.toFile().getAbsolutePath(), SOURCE_DIR);
+            if (!noTMX) {
+                try {
+                    extractTM(projectDir);
+                } catch (Exception e) {
+                    LOGGER.log(Level.WARNING, "Error extracting SDLTM", e);
+                }
+            }
+
+            if (!noGlossary) {
+                try {
+                    extractGlossaries(projectDir);
+                } catch (Exception e) {
+                    LOGGER.log(Level.WARNING, "Error extracting SDLTB", e);
+                }
+            }
+
+            if (noSource) {
+                return true;
+            }
+
+            File sourceDir = new File(projectDir, SOURCE_DIR);
             sourceDir.mkdirs();
 
             // We assume the target directories are always flat with sdlxliff?
-            Files.find(zipfs.getPath(targetLanguage), 1,
-                    (path, basicFileAttributes) -> path.getFileName().toString().endsWith(".sdlxliff"))
+            PathMatcher matcher2 = FileSystems.getDefault().getPathMatcher("glob:*.sdlxliff");
+            Files.find(zipfs.getPath(targetLanguage), MAX_DEPTH, (path, basicFileAttributes) -> matcher2.matches(path))
                     .forEach(actionPath -> {
                         Path source = Paths.get(sourceDir.getAbsolutePath(), actionPath.getFileName().toString());
-                        LOGGER.log(Level.INFO, "Copy {0} > {1}", new Object[] { actionPath, source });
+                        LOGGER.log(Level.INFO, "Copy source file {0} to {1}", new Object[] { actionPath, source });
                         try {
                             Files.copy(actionPath, source, StandardCopyOption.REPLACE_EXISTING);
                         } catch (IOException e) {
@@ -131,8 +236,45 @@ public class SDLPPXPackager {
         return true;
     }
 
-    public boolean updateSdlppx() throws IOException, TransformerConfigurationException, ParserConfigurationException,
-            SAXException, TransformerFactoryConfigurationError, TransformerException {
+    private void extractGlossaries(String projectDir) throws Exception {
+        PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:*.sdltb");
+        try (FileSystem zipfs = FileSystems.newFileSystem(sdlPpx, null)) {
+            List<Path> sdltmFiles = Files.find(zipfs.getRootDirectories().iterator().next(), MAX_DEPTH,
+                    (path, basicFileAttributes) -> matcher.matches(path)).collect(Collectors.toList());
+            LOGGER.log(Level.INFO, "SDLTB file: {0} found", sdltmFiles.size());
+            for (Path sdltm : sdltmFiles) {
+                File tmpFile = File.createTempFile("sdlppx_", ".sdltb");
+                FileOutputStream fos = new FileOutputStream(tmpFile);
+                Files.copy(sdltm, fos);
+                fos.close();
+                String glossaryPrefix = sdlPpx.getFileName().toString().replaceFirst("\\.\\w+$", "");
+                new SDLTBConverter().convertSDLTB(tmpFile, new File(projectDir, GLOSSARY_DIR), glossaryPrefix);
+                tmpFile.delete();
+            }
+        }
+
+    }
+
+    private void extractTM(String projectDir) throws Exception {
+        PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:*.sdltm");
+        try (FileSystem zipfs = FileSystems.newFileSystem(sdlPpx, null)) {
+            List<Path> sdltmFiles = Files.find(zipfs.getRootDirectories().iterator().next(), 10,
+                    (path, basicFileAttributes) -> matcher.matches(path)).collect(Collectors.toList());
+            LOGGER.log(Level.INFO, "SDLTM file: {0} found", sdltmFiles.size());
+            for (Path sdltm : sdltmFiles) {
+                File tmpFile = File.createTempFile("sdlppx_", ".sdltm");
+                FileOutputStream fos = new FileOutputStream(tmpFile);
+                Files.copy(sdltm, fos);
+                fos.close();
+                new SDLTMConverter().convertSDLTM(tmpFile, new File(projectDir, TM_DIR));
+                tmpFile.delete();
+            }
+        }
+
+    }
+
+    public boolean updateSdlppx(String projectDir) throws IOException, TransformerConfigurationException,
+            ParserConfigurationException, SAXException, TransformerFactoryConfigurationError, TransformerException {
 
         Map<String, String> env = new HashMap<>();
         env.put("create", "false");
@@ -155,8 +297,7 @@ public class SDLPPXPackager {
             Files.find(zipfs.getPath(targetLanguage), 1,
                     (path, basicFileAttributes) -> path.getFileName().toString().endsWith(".sdlxliff"))
                     .forEach(actionPath -> {
-                        Path source = Paths.get(projectDir.toFile().getAbsolutePath(), TARGET_DIR,
-                                actionPath.getFileName().toString());
+                        Path source = Paths.get(projectDir, TARGET_DIR, actionPath.getFileName().toString());
                         LOGGER.log(Level.INFO, "Replace {0} > {1}", new Object[] { source, actionPath });
                         try {
                             Files.copy(source, actionPath, StandardCopyOption.REPLACE_EXISTING);
@@ -179,20 +320,18 @@ public class SDLPPXPackager {
             throws ParserConfigurationException, SAXException, TransformerConfigurationException,
             TransformerFactoryConfigurationError, IOException, TransformerException {
 
-        final DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
         final DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
         // Important, the DocumentBuilder must be created with an InputStream, otherwise
         // the zip can't be updated as the stream is not explicitely closed.
-        Document doc;
         try (InputStream is = Files.newInputStream(sdlProj, StandardOpenOption.READ)) {
-            doc = docBuilder.parse(is);
+            sdlProjDoc = docBuilder.parse(is);
         } catch (IOException e) {
             return false;
         }
 
         // Language code, obviously we don't deal with multiple target languages.
         // /PackageProject/LanguageDirections/LanguageDirection/@TargetLanguageCode="fr-FR"
-        targetLanguage = ((Element) ((Element) ((Element) doc.getElementsByTagName("PackageProject").item(0))
+        targetLanguage = ((Element) ((Element) ((Element) sdlProjDoc.getElementsByTagName("PackageProject").item(0))
                 .getElementsByTagName("LanguageDirections").item(0)).getElementsByTagName("LanguageDirection").item(0))
                         .getAttribute("TargetLanguageCode");
 
@@ -202,13 +341,13 @@ public class SDLPPXPackager {
             return false;
         }
 
-        final Node attrPackageType = doc.getDocumentElement().getAttributeNode(ATTRIBUTE_PACKAGE_TYPE);
+        final Node attrPackageType = sdlProjDoc.getDocumentElement().getAttributeNode(ATTRIBUTE_PACKAGE_TYPE);
         final PackageTypes packageType = PackageTypes.valueOf(attrPackageType.getTextContent());
         switch (packageType) {
         case ProjectPackage:
             LOGGER.info("This is a project package. Changing to ReturnPackage");
             attrPackageType.setNodeValue(PackageTypes.ReturnPackage.toString());
-            updateDoc(sdlProj, doc);
+            updateDoc(sdlProj, sdlProjDoc);
             return true;
 
         case ReturnPackage:
@@ -248,6 +387,30 @@ public class SDLPPXPackager {
         }
 
         Files.copy(tmpSdlProj, sdlProj, StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    public boolean isNoTMX() {
+        return noTMX;
+    }
+
+    public void setNoTMX(boolean noTMX) {
+        this.noTMX = noTMX;
+    }
+
+    public boolean isNoGlossary() {
+        return noGlossary;
+    }
+
+    public void setNoGlossary(boolean noGlossary) {
+        this.noGlossary = noGlossary;
+    }
+
+    public boolean isNoSource() {
+        return noSource;
+    }
+
+    public void setNoSource(boolean noSource) {
+        this.noSource = noSource;
     }
 
 }
